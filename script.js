c69ab1fb108107dd0b75ba645df3b539fbb762a7
Wei-Liang, Lin <39706411+Eric0911@users.noreply.github.com>
2025-06-07 19:49:35 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,27 @@ function toggleDropdown() {
     document.querySelector('.dropdown').classList.toggle('show');
   }
 
-  // 點擊頁面其他地方時自動關閉選單
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-button')) {
-      let dropdowns = document.getElementsByClassName("dropdown");
-      for (let i = 0; i < dropdowns.length; i++) {
-        dropdowns[i].classList.remove('show');
-      }
-    }
-  }
+// function changeImage() {
+//   // const img = document.getElementById("myImage");
+//   const img = document.getElementsByClassName("like_numbers");
+//   const filename = img.src.split('/').pop();
+
+//   if (filename === "like_20x20.png") {
+//     img.src = "./image/heart_20x20.png";
+//   } else {
+//     img.src = "./image/like_20x20.png";
+//   }
+// }
+
+  // 抓取所有有 toggle-img class 的圖片
+  const toggleImages = document.querySelectorAll('.toggle-img');
+
+  toggleImages.forEach(img => {
+    img.addEventListener('click', () => {
+      const currentSrc = img.src.split('/').pop();
+      const img1 = img.getAttribute('data-img1').split('/').pop();
+      const img2 = img.getAttribute('data-img2');
+
+      img.src = (currentSrc === img1) ? img2 : img.getAttribute('data-img1');
+    });
+  });
